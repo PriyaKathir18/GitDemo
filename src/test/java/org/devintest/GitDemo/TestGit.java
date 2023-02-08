b3ef5095b1f9908a16844e226d7d3e39c9f8bf7c
PriyaKathir18 <krishpriyaa@gmail.com>
2023-02-08 11:54:31 +0000

    First Git Try

---
+++ src/test/java/org/devintest/GitDemo/TestGit.java
@@ -0,0 +1,12 @@
+package org.devintest.GitDemo;
+
+import org.junit.Test;
+
+public class TestGit {
+
+    @Test
+    public void test()
+    {
+        System.out.println("Executing Test");
+    }
+}
